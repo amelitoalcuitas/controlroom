6a39a6a9fc41df90ab0900823a1a84de2a7a405a
amelitoalcuitas <amelitoalcuitasjr@gmail.com>
2016-09-28 02:39:08 +0800

    added siteheader.php

---
+++ students/siteheader.php
@@ -0,0 +1,49 @@
+<!-- Logo -->
+    <a href="index.html" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>ST</b>P</span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><b>Student</b>Panel</span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <li class="dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+             
+              <span class="hidden-xs"><?php echo $userin; ?></span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- User image -->
+              <li class="user-header">
+                <img src="User_images/me.jpg" class="img-circle" alt="User Image">
+               
+                <p>
+                  <?php echo $userin; ?>
+                   <br> Student
+                </p>
+              </li>
+              <!-- Menu Body -->
+              <!-- Menu Footer-->
+              <li class="user-footer">
+                <div class="pull-right">
+                  <a href="S_logout.php" class="btn btn-default btn-flat">Sign out</a>
+                </div>
+                <div class="pull-left">
+                  <a href="S_profile.php" class="btn btn-default btn-flat">Profile</a>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
